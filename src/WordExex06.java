f0cbdf3d22a593d298e7004aa10e8857d7153f35
Mini01 <skekdwoals@naver.com>
2018-11-30 15:23:31 +0900

    first

---
+++ src/WordExex06.java
@@ -0,0 +1,13 @@
+
+public class WordExex06 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		WordService ws = new WordService();
+		
+		ws.makeWordList();
+		ws.playGame();
+
+	}
+
+}
